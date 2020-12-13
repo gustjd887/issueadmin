0a84ffe0a17e00229e01da0fce2d183e136cf69b
gustjd887 <gustjd887@gmail.com>
2020-12-13 12:01:16 +0900

    change allow host ip

--- issuesite/settings.py
+++ issuesite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['125.184.216.70']
 
 
 # Application definition
